e5cb565f11444e670aa60cb8705618e7775fc976
NP <nastya.pashik@gmail.com>
2022-04-22 14:47:03 +0300

    Update streamlit_app.py
    add variable for user choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,10 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The User Entered', fruit_choice)
+
+fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 fruityvice_normalize = pandas.json_normalize(fruityvice_responce.json())
 streamlit.dataframe(fruityvice_normalize)
