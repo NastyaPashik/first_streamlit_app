07209fd3819e6f202ac2645c42dd0bdbbdf05380
NP <nastya.pashik@gmail.com>
2022-04-25 20:30:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def get_fruityvice_data(this_fruit_choice):
   
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
@@ -53,7 +53,7 @@ if streamlit.button('Get Fruit Load List'):
     with my_cnx.cursor() as my_cur:
       my_cur.execute("insert into fruit_load_list values ('test')")
     return "Thanks for adding " + new_fruit
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Kiwi')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
